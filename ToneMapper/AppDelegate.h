9070c30a21bcf5137fc74f21040a81bb620e1855
Hagai Weinfeld <hagai@lightricks.com>
2015-09-20 18:43:42 +0300

    Initial Commit

---
+++ ToneMapper/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ToneMapper
+//
+//  Created by Hagai Weinfeld on 9/20/15.
+//  Copyright (c) 2015 Lightricks Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ToneMapper/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ToneMapper
+//
+//  Created by Hagai Weinfeld on 9/20/15.
+//  Copyright (c) 2015 Lightricks Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
