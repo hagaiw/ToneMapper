38f60233f5616a4c36145d201bd3303911910dd5
Hagai Weinfeld <hagai@lightricks.com>
2015-10-19 11:15:42 +0300

    More refactoring

---
+++ ToneMapper/TMGeometryFactory.h
@@ -0,0 +1,13 @@
+//
+//  TMGeometryFactory.h
+//  ToneMapper
+//
+//  Created by Hagai Weinfeld on 10/19/15.
+//  Copyright (c) 2015 Lightricks Ltd. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TMGeometryFactory : NSObject
+
+@end
---
+++ ToneMapper/TMGeometryFactory.m
@@ -0,0 +1,13 @@
+//
+//  TMGeometryFactory.m
+//  ToneMapper
+//
+//  Created by Hagai Weinfeld on 10/19/15.
+//  Copyright (c) 2015 Lightricks Ltd. All rights reserved.
+//
+
+#import "TMGeometryFactory.h"
+
+@implementation TMGeometryFactory
+
+@end
